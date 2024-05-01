7ca8fee37ae1ed675040e36ddea90150d691ed37
MohammadAbdulSamad1 <samadmohammadabdul71@gmail.com>
2024-05-01 20:44:48 +0500

    additional new js files

---
+++ js/calculator.js
@@ -0,0 +1,4 @@
+let a=prompt('enter first number to Add');
+let b=prompt('enter Second number to Add');
+let c= Number(a)+Number(b);
+console.log('the addition is '+c);
---
+++ js/templete.js
@@ -0,0 +1,6 @@
+var a=20;
+let b=30;
+console.log(a,b);
+var a=10; // a can be redecleared using var keyword
+// let b=50;         b is already decleared in using let
+console.log(a,b);
